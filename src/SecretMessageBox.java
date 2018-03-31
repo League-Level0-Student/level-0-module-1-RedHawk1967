52dd3a08ca2a3de1b647d66b611b99dc32412138
Jack <league@WTS5.attlocal.net>
2018-03-30 18:25:39 -0700

    m1

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -1,31 +1,41 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
+import java.net.PasswordAuthentication;
 
 import javax.swing.JOptionPane;
 
 /**
  * Secret Message Box / Secure Messaging System
  * 
- * You want to leave a message on one of the Mac computers so that only your friend can read it.
- * You set up the passcode and the secret message. 
- * Your friend types in the passcode to retrieve the message.
+ * You want to leave a message on one of the Mac computers so that only your
+ * friend can read it. You set up the passcode and the secret message. Your
+ * friend types in the passcode to retrieve the message.
  * 
  */
 
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
-	
-	// 1. Set a password in a String variable
-	
-	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
+	public static void main(String[] args) {
+		String bob = "123";
 
-	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
-	//    if they can guess the passcode
+		// 1. Set a password in a String variable
+String bob3 = JOptionPane.showInputDialog("ENETR SECRET MESSAGE");
+		// 2. Using a pop-up, ask the first person for a secret message and store it in
+		// a variable
+		String bob2 = JOptionPane.showInputDialog(null, "ENTER THE PASSWORD FOR SECRET MESSAGE");
+		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret
+		// message
+		// if they can guess the passcode
 
-	// 4. If their guess matches the password, show them the secret message
+		// 4. If their guess matches the password, show them the secret message
+		if (bob2 .equals(bob)){
+			JOptionPane.showMessageDialog(null, "SHOWING SECRET MESSAGE");
+			// 5. If the password does not match, pop-up "INTRUDER!!"
+		JOptionPane.showMessageDialog(null, bob3);} else {JOptionPane.showMessageDialog(null,"GUARDS, INTRUDERS" );
 
-	// 5. If the password does not match, pop-up "INTRUDER!!"
-
+		}
+	}
 }
--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,24 +1,44 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int number = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		String awnser = JOptionPane.showInputDialog(
+				"Here's a riddle A dad and his son were riding their bikes and crashed. Two ambulances came and took them to different hospitals. The man’s son was in the operating room and the doctor said, “I can’t operate on you. You’re my son.”\n"
+						+ "Who is the doctor?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		JOptionPane.showMessageDialog(null, "The Doctor is his mom");
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+		if (awnser.equalsIgnoreCase("the doctor is his mom")) {
+			number++;
+			JOptionPane.showMessageDialog(null,
+					"nice,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  DID YOU CHEAT     ");
+			JOptionPane.showMessageDialog(null, "Score:" + number);
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong"); JOptionPane.showMessageDialog(null, "Score:" + number);
+		}
 		// 6. Add some more riddles
-
+		String awnser2 = JOptionPane.showInputDialog(null,
+				"In a one-story pink house, there was a pink person, a pink cat, a pink fish, a pink computer, a pink chair, a pink table, a pink telephone, a pink shower– everything was pink!\n"
+						+ "What color were the stairs?");
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null, "There were no stairs");
+		if (awnser2.equalsIgnoreCase("There were no stairs")) {
+			number++;
+			JOptionPane.showMessageDialog(null, "Nice you got it");
+			JOptionPane.showMessageDialog(null, "Score:" + number);
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong"); JOptionPane.showMessageDialog(null, "Score:" + number);
+		}
+
 	}
 }
-
